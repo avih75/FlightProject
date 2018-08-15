eae386134f69401bf2e6dd4ee38247f2939e57d7
avih75 <35527203+avih75@users.noreply.github.com>
2018-08-15 18:22:18 +0300

    second

---
+++ Common/Models/StationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public class StationModel : IStation
+    {
+        public int StationId { get; set; }
+
+        private bool IsOccupied { get; set; }
+
+        public FlightModel FlightInStation { get; set; }
+
+        private List<StationModel> NextTakeOffStations { get; set; }
+
+        private List<StationModel> NextLandingStations { get; set; }
+
+        private LinkedList<StationModel> StationWaitingList { get; set; }
+
+        public void SendFlightToMe()
+        {
+            IsOccupied = false;
+            FlightInStation = null;
+            // action of movment in the server
+        }
+    }
+}
