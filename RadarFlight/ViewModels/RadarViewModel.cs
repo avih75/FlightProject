34b471d26c1966a66ca7123f3dd7da147ed268f0
avih75 <35527203+avih75@users.noreply.github.com>
2018-08-28 23:02:10 +0300

    wel

--- RadarFlight/ViewModels/RadarViewModel.cs
+++ RadarFlight/ViewModels/RadarViewModel.cs
@@ -13,15 +13,13 @@ namespace RadarFlight.ViewModels
     {
         private List<StationModel> StationsList { get; set; }
         public List<FlightModel> FlightList { get; set; }
-        //FirstStationModel startStationProp {get;set;notify();}
-
-        public ICommand StartTheRadarClick;
 
         public RadarViewModel(Grid inMotion)
         {
             CreatFakeDataBase();
             GetHubConection();
-            CommandInit();
+            GetDataToShow();
+
         }
 
         private void CreatFakeDataBase()
@@ -43,74 +41,60 @@ namespace RadarFlight.ViewModels
                 IsDeparture = false,
                 Time = DateTime.Now
             });
-
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-            StationsList.Add(new StationModel());
-        }
-
-        private void CommandInit()
-        {
-            //StartTheRadarClick = new ICommand(MakeFirstStateCall);
         }
 
         private void GetHubConection()
         {
             // get the contact to the hub
+            // sing to get change for flight and call make move
         }
 
-        //private void MakeFirstStateCall()
-        //{
-        //    //if (connection)
-        //    {
-        //        // get the station list
-        //        // get the second list
-        //        GenerateView();
-        //    }
-        //}
-
-        //private void GenerateView()
-        //{
-        //    int MaxStartStation;// = bigest base list count
-        //    int LongestPath;// =  biggest path form base to end of two ways
-        //    //Canvas pathsCanvas = CreatCanvasForView(LongestPath, MaxStartStation);
-        //    FillCanvas(pathsCanvas);
-        //    PutFlights();
-        //}
-
-        //private Grid CreatCanvasForView(int x, int y)
-        //{
-        //    // generate grid whit coloms and rows by the sizes
-        //}
-
-        //private void FillCanvas(Canvas pathsCanvas)
-        //{
-        //    foreach (var item in collection)
-        //    {
-        //        // put the base station A
-        //        // and wallk the all paths and put station
-        //    }
-
-        //    foreach (var item in collection)
-        //    {
-        //        // put the base station B
-        //    }
-        //}
-
-        //private void PutFlights()
-        //{
-        //    foreach (var item in collection)
-        //    {
-        //        // put all flights
-        //    }
-        //}
-
-        private void MakeMove()
+        private void GetDataToShow()
         {
+            //make get http request for flight lisdt
+            // make get http request for stationlist
+
+        }
+
+        private void GenerateGrid()
+        {
+            int MaxStartStation = 0;// = bigest base list count
+            int LongestPath = 0;// =  biggest path form base to end of two ways
+            Canvas pathsCanvas = CreatCanvasForView(LongestPath, MaxStartStation);
+            FillCanvas(pathsCanvas);
+            PutFlights();
+        }
+
+        private Canvas CreatCanvasForView(int longestPath, int maxStartStation)
+        {
+            return null;
+        }
+
+        private void FillCanvas(Canvas pathsCanvas)
+        {
+            //foreach (var item in collection)
+            //{
+            //    // put the base station A
+            //    // and wallk the all paths and put station
+            //}
+
+            //foreach (var item in collection)
+            //{
+            //    // put the base station B
+            //}
+        }
+
+        private void PutFlights()
+        {
+            //foreach (var item in collection)
+            //{
+            //    // put all flights
+            //}
+        }
+
+        private void MakeMove(FlightModel flight, int stationId)
+        {
+            //
             // make move of flight picture to the new station
             // update Dictionery Flights
         }
